f64844cd30fbb5bec210ae7b500b513d9d7433a4
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-11-20 00:05:02 -0300

    log de token

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -1,12 +1,22 @@
-
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import LocalStorageService from '../services/localStorageService';
+import SecurityService from '../services/securityService';
 import { AuthGuardProps } from '../interfaces/AuthGuardProps';
 
 const AuthGuard: React.FC<AuthGuardProps> = ({ children }) => {
-  const isLoggedIn = LocalStorageService.hasItem('token');
+  const token = LocalStorageService.getItem('token');
 
-  return isLoggedIn ? children : <Navigate to="/access-denied" />;
+  // Verificação se o token é válido
+  if (token) {
+    if (SecurityService.isTokenExpired(token)) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  }
+
+  // Se o token não existir, redirecionar para a tela de acesso negado
+  return <Navigate to="/access-denied" />;
 };
+
 export default AuthGuard;
--- src/services/securityService.ts
+++ src/services/securityService.ts
@@ -1,5 +1,6 @@
 import { jwtDecode, JwtPayload } from 'jwt-decode';
 import LocalStorageService from './localStorageService';
+import DataHelper from '../helpers/dataUtilshelper';
 
 class SecurityService {
     static isTokenValid(token: string | null): boolean {
@@ -9,8 +10,11 @@ class SecurityService {
         try {
             if (token !== null) {
                 const decoded = jwtDecode<JwtPayload>(token);
-                console.log('Decoded token:', decoded); 
+                console.log('Decoded token:', decoded);
                 const currentTime = Date.now() / 1000; 
+                if (decoded.exp) {
+                    console.log('Token expiration time:', DataHelper.formatTimestamp(decoded.exp));
+                }
                 return decoded.exp ? decoded.exp > currentTime : false;
             }
             console.log('Token is null');
@@ -20,6 +24,10 @@ class SecurityService {
             console.error('Error decoding token:', error);
             return false;
         }
+    }
+
+    static isTokenExpired(token: string): boolean {
+        return !this.isTokenValid(token);
     }
 
     static getToken(): string | null {
