882463508f08532602bf3fa7fbe2603783c8583d
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-11-23 20:25:33 -0300

    Revert ajust

--- src/FooterPage.tsx
+++ src/FooterPage.tsx
@@ -45,7 +45,42 @@ const FooterPage: React.FC = () => {
             <img src={viteLogo} className="logo" alt="Vite" title='Vite' />
           </a>
           <div className="server-info">
-            {/* Os outros ícones e links aqui */}
+            <a href="https://getbootstrap.com/" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" alt="Bootstrap" title="Bootstrap" />
+            </a>
+            <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="TypeScript" title="TypeScript" />
+            </a>
+            <a href="https://nodejs.org/" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="Node.js" title="Node.js" />
+            </a>
+            <a href="https://github.com/LeoneRocha/HotelWiseUI" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg " alt="GitHub" title="GitHub" className="white-icon" />
+            </a>
+            <a href="https://www.nginx.com/" target="_blank" rel="noopener noreferrer">
+              <img src="https://static.cdnlogo.com/logos/n/17/nginx.svg" alt="Nginx Server" title="Nginx Server" />
+            </a>
+            <a href="https://sonarcloud.io/summary/new_code?id=lionscorp_hotelwiseui" target="_blank" rel="noopener noreferrer">
+              <img src="https://static.cdnlogo.com/logos/s/13/sonarcloud.svg" alt="SonarCloud" title="SonarCloud" />
+            </a>
+            <a href="https://hub.docker.com/r/leonecr/hotelwiseui" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original-wordmark.svg" alt="Docker Hub" title="Docker Hub" />
+            </a>
+            <a href="https://azure.microsoft.com/" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original-wordmark.svg" alt="Azure Cloud" title="Azure Cloud" />
+            </a>
+            <a href="https://lionscorp.visualstudio.com/VariousStudies/_build" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azuredevops/azuredevops-original.svg" alt="Azure DevOps" title="Azure DevOps" />
+            </a>
+            <a href="https://qdrant.tech/" target="_blank" rel="noopener noreferrer">
+              <img src="https://qdrant.tech/img/brand-resources-logos/logo.svg" alt="qdrant" title="qdrant" />
+            </a>
+            <a href="https://mistral.ai/" target="_blank" rel="noopener noreferrer">
+              <img src="https://custom.typingmind.com/assets/models/mistralai.png" alt="mistral" title="mistral" />
+            </a>
+            <a href="https://groq.com/" target="_blank" rel="noopener noreferrer">
+              <img src="https://groq.com/wp-content/uploads/2024/03/PBG-mark1-color.svg" alt="Powered by Groq for fast inference." title="Powered by Groq for fast inference." className="white-icon" />
+            </a>
           </div>
         </div>
         <br />
