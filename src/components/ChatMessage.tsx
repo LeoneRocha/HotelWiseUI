7e2c37196b2ee9d24ffe157af05ce0a5c49e8b38
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-11-28 23:55:39 -0300

    Acessibilidade

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -4,15 +4,15 @@ import { FaRobot, FaUser } from 'react-icons/fa';
 import { IChatMessageProps } from '../interfaces/IChatbotModalProps'; 
 
 const ChatMessage: React.FC<IChatMessageProps> = ({ message }) => (
-  <div className={`chat-message ${message.sender}`}>
+  <div className={`chat-message ${message.sender}`} role="alert" aria-live="assertive">
     {message.sender === 'bot' ? (
       <div className="bot-message">
-        <FaRobot className="message-icon" />
+        <FaRobot className="message-icon" aria-label="Mensagem do assistente" />
         <div className="message-content" dangerouslySetInnerHTML={{ __html: message.text }} />
       </div>
     ) : (
       <div className="user-message">
-        <FaUser className="message-icon" />
+        <FaUser className="message-icon" aria-label="Mensagem do usuário" />
         <div className="message-content">{message.text}</div>
       </div>
     )}
