018467e1287b1e8c22565ff3666664b48835b675
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-12-26 16:12:46 -0300

    Ajuste sonar cloud lote 4

--- src/components/HotelSearchTemplate.tsx
+++ src/components/HotelSearchTemplate.tsx
@@ -49,54 +49,53 @@ const HotelSearchTemplate: React.FC<IHotelSearchTemplateProps> = ({
   );
 
   const renderHotels = !loading && searched && !error && serviceResponse && (
-    <>
-      <div className="row w-100">
-        {serviceResponse?.data.hotelsVectorResult.length > 0 && (
-          serviceResponse.data.hotelsVectorResult.map(hotel => (
-            <div key={hotel.hotelId} className="col-md-4 mb-4">
-              <div className="card h-100">
-                <div className="card-img-top d-flex justify-content-center align-items-center" style={{ height: '200px', backgroundColor: '#f8f9fa' }}>
-                  <i className="fas fa-hotel fa-4x text-muted"></i>
-                </div>
-                <div className="card-body">
-                  <h5 className="card-title">{hotel.hotelName}
-                    <i className="fas fa-info-circle text-muted" title={`Pontuação: ${hotel.score}`} style={{ marginLeft: '10px' }}></i>
-                  </h5>
-                  <p className="card-text"><strong>Descrição:</strong> {hotel.description}</p>
-                  <p className="card-text"><strong>Tags:</strong> {hotel.tags.join(', ')}</p>
-                  <p className="card-text"><strong>Estrelas:</strong> {renderStars(hotel.stars)}</p>
-                  <p className="card-text"><strong>Preço Inicial:</strong> R${hotel.initialRoomPrice.toFixed(2)}</p>
-                  <p className="card-text"><strong>Localização:</strong> {hotel.location}, {hotel.city} - {hotel.stateCode}</p>
-                  <p className="card-text"><strong>CEP:</strong> {hotel.zipCode}</p>
-                </div>
+
+    <div className="row w-100">
+      {serviceResponse?.data.hotelsVectorResult.length > 0 && (
+        serviceResponse.data.hotelsVectorResult.map(hotel => (
+          <div key={hotel.hotelId} className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-img-top d-flex justify-content-center align-items-center" style={{ height: '200px', backgroundColor: '#f8f9fa' }}>
+                <i className="fas fa-hotel fa-4x text-muted"></i>
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">{hotel.hotelName}
+                  <i className="fas fa-info-circle text-muted" title={`Pontuação: ${hotel.score}`} style={{ marginLeft: '10px' }}></i>
+                </h5>
+                <p className="card-text"><strong>Descrição:</strong> {hotel.description}</p>
+                <p className="card-text"><strong>Tags:</strong> {hotel.tags.join(', ')}</p>
+                <p className="card-text"><strong>Estrelas:</strong> {renderStars(hotel.stars)}</p>
+                <p className="card-text"><strong>Preço Inicial:</strong> R${hotel.initialRoomPrice.toFixed(2)}</p>
+                <p className="card-text"><strong>Localização:</strong> {hotel.location}, {hotel.city} - {hotel.stateCode}</p>
+                <p className="card-text"><strong>CEP:</strong> {hotel.zipCode}</p>
               </div>
             </div>
-          ))
-        )}
-        {serviceResponse?.data.hotelsIAResult.length > 0 && (
-          serviceResponse.data.hotelsIAResult.map(hotel => (
-            <div key={hotel.hotelId} className="col-md-4 mb-4">
-              <div className="card h-100">
-                <div className="card-img-top d-flex justify-content-center align-items-center" style={{ height: '200px', backgroundColor: '#f8f9fa' }}>
-                  <i className="fas fa-hotel fa-4x text-muted"></i>
-                </div>
-                <div className="card-body">
-                  <h5 className="card-title">{hotel.hotelName}
-                    <i className="fas fa-info-circle text-muted" title={`Pontuação: ${hotel.score}`} style={{ marginLeft: '10px' }}></i>
-                  </h5>
-                  <p className="card-text"><strong>Descrição:</strong> {hotel.description}</p>
-                  <p className="card-text"><strong>Tags:</strong> {hotel.tags.join(', ')}</p>
-                  <p className="card-text"><strong>Estrelas:</strong> {renderStars(hotel.stars)}</p>
-                  <p className="card-text"><strong>Preço Inicial:</strong> R${hotel.initialRoomPrice.toFixed(2)}</p>
-                  <p className="card-text"><strong>Localização:</strong> {hotel.location}, {hotel.city} - {hotel.stateCode}</p>
-                  <p className="card-text"><strong>CEP:</strong> {hotel.zipCode}</p>
-                </div>
+          </div>
+        ))
+      )}
+      {serviceResponse?.data.hotelsIAResult.length > 0 && (
+        serviceResponse.data.hotelsIAResult.map(hotel => (
+          <div key={hotel.hotelId} className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-img-top d-flex justify-content-center align-items-center" style={{ height: '200px', backgroundColor: '#f8f9fa' }}>
+                <i className="fas fa-hotel fa-4x text-muted"></i>
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">{hotel.hotelName}
+                  <i className="fas fa-info-circle text-muted" title={`Pontuação: ${hotel.score}`} style={{ marginLeft: '10px' }}></i>
+                </h5>
+                <p className="card-text"><strong>Descrição:</strong> {hotel.description}</p>
+                <p className="card-text"><strong>Tags:</strong> {hotel.tags.join(', ')}</p>
+                <p className="card-text"><strong>Estrelas:</strong> {renderStars(hotel.stars)}</p>
+                <p className="card-text"><strong>Preço Inicial:</strong> R${hotel.initialRoomPrice.toFixed(2)}</p>
+                <p className="card-text"><strong>Localização:</strong> {hotel.location}, {hotel.city} - {hotel.stateCode}</p>
+                <p className="card-text"><strong>CEP:</strong> {hotel.zipCode}</p>
               </div>
             </div>
-          ))
-        )}
-      </div>
-    </>
+          </div>
+        ))
+      )}
+    </div>
   );
 
   const renderAlert = serviceResponse && serviceResponse.errors.length > 0 && showAlert && (
